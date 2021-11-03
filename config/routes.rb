f32f24270cb8d6aa274400251c277dfd59ff0f88
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2021-11-03 15:16:53 -0500

    auth done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  post '/auth/login', to: 'authentications#login'
+  get '/auth/verify', to: 'authentications#verify'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
