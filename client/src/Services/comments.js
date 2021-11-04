16d96b7956e05d6bd6056e7af1d072f7cd911352
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2021-11-04 17:48:58 -0500

    made posts api calls file

--- client/src/Services/comments.js
+++ client/src/Services/comments.js
@@ -1,7 +1,21 @@
-export default function comments() {
-  return (
-    <div>
-      
-    </div>
-  )
+import api from './apiConfig'
+
+export const getAllComments = async () => {
+  const resp = await api.get('/comments')
+  return resp.data
+}
+
+export const getOneComment = async (id) => {
+  const resp = await api.get(`/comments/${id}`)
+  return resp.data
+}
+
+export const makeComment = async (commentData) => {
+  const resp = await api.post('/comments', {comment: commentData})
+  return resp.data
+}
+
+export const makeComment = async (id, commentData) => {
+  const resp = await api.put(`/comments/${id}`, {comment: commentData})
+  return resp.data
 }
--- client/src/Services/posts.js
+++ client/src/Services/posts.js
@@ -1,7 +1,16 @@
-export default function posts() {
-  return (
-    <div>
-      
-    </div>
-  )
+import api from './apiConfig'
+
+export const getAllPosts = async () => {
+  const resp = await api.get('/posts')
+  return resp.data
+}
+
+export const getOnePost = async (id) => {
+  const resp = await api.get(`/posts/${id}`)
+  return resp.data
+}
+
+export const makePost = async (postData) => {
+  const resp = await api.post('/posts', {post: postData})
+  return resp.data
 }
