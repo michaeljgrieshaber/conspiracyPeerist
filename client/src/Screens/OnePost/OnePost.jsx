3ccdbc996ace26c1d2a19a8fe3f99fb9caec5149
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2021-11-05 13:01:12 -0500

    changed posts controller to return comments/users in more than just a single post

--- client/src/Screens/OnePost/OnePost.jsx
+++ client/src/Screens/OnePost/OnePost.jsx
@@ -1,19 +1,35 @@
-import {useParams, useEffect} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import {useState, useEffect} from 'react'
 
 
 export default function OnePost(props) {
   const { posts } = props
   const { id } = useParams();
+  const [post, setPost] = useState(null)
   
- 
+  console.log(posts)
 
-  const post = posts.find(element => element.id === Number(id))
+  useEffect(() => {
+  
+    setPost(posts.find(element => element.id === Number(id)))
 
+
+
+    },[])
+
+  // const post = posts.find(element => element.id === Number(id))
+
+
+
+  console.log(post)
+  
+  
 
   return (
     <div>
       <p>one post</p>
-      <p>{post.title}</p>
+      {/* <p>{post.title}</p> */}
+      {/* <p>By: {post.user.username}</p> */}
     </div>
   )
 }
