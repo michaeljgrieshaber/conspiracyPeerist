216c7e025d74640df0b2557e292726060d77ab48
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2021-11-07 11:22:02 -0600

    posts css

--- client/src/Screens/Posts/Posts.jsx
+++ client/src/Screens/Posts/Posts.jsx
@@ -1,24 +1,28 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import './posts.css'
 
 export default function Posts(props) {
   const { posts } = props
 
   function verify() {
     if (localStorage.length === 1) {
-      return <Link to='/makepost'>Create a post</Link>
+      return <Link to='/makepost' className='makePostLink'>Create a post</Link>
       } else {
     }
   }
   
   return (
     <div>
-      <h2>Posts</h2>
+      <div className='postsHeader'>Posts</div>
       {verify()}
       {posts.map((post) => (
         <div key={`post${post.id}`}>
-          <Link to={`/posts/${post.id}`}>
-            <p>{post.title}</p>
+            <div className='postCardContainer'>
+            <Link to={`/posts/${post.id}`}
+            className='postCardLink'>
+            <div className='postCard'>{post.title}</div>
           </Link>
+            </div>
         </div>
       ))}
     </div>
